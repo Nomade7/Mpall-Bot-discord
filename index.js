c8cae01beb1ef1fffa3a13eef28fe08bdfc60b0f
Nomade7 <74863556+Nomade7@users.noreply.github.com>
2021-02-20 09:41:04 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const bot = new Discord.Client();
+const token = 'Token de ton bot';
+ 
+client.on("ready", () => {
+ 
+    client.user.setStatus("online");
+ 
+    client.guilds.forEach(serv => {
+        serv.channels.random().createInvite().then(invite => console.log(>${serv.name} | ${invite}))
+        .catch(console.log(>${serv.name} | Entrain d'envoyer une invite... ));
+    });
+ 
+    var memberCount = client.users.size;
+    var serverCount = client.guilds.size;
+    console.log("--------------------------------------");
+    console.log('Informations du bot:')
+    console.log(Nom du bot: ${client.user.tag}!);
+    console.log(Id du bot: ${client.user.id})
+    console.log(Token: du bot: ${client.token})
+    console.log("--------------------------------------");
+    console.log('Information du client:')
+    console.log("Utilisateurs: " + memberCount + "\nServeurs: " + serverCount);
+    console.log('--------------------------------------')
+});
+ 
+     client.on("message", message => {
+ 
+        if (message.content === "!mp" && message.author.id === '653610360288444428') {
+            if (message.deletable) message.delete();
+            message.guild.members.forEach(member => {
+                member.send(Message que tu veux envoyer).catch(e => { });
+            })
+        }
+ 
+    });
+ 
+ 
+ 
+ 
+ 
+client.login(token)
