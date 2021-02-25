9bef56ea42972cb2cd064c961bcdea1b374aa358
Nomade7 <74863556+Nomade7@users.noreply.github.com>
2021-02-25 09:34:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const bot = new Discord.Client();
-const token = 'Token de ton bot';
+const config = require('./config.json')
+const color = require("colors")
+const setTitle = require('console-title');
+const token = config.token;
  
 client.on("ready", () => {
- 
-    client.user.setStatus("online");
- 
-    client.guilds.forEach(serv => {
-        serv.channels.random().createInvite().then(invite => console.log(>${serv.name} | ${invite}))
-        .catch(console.log(>${serv.name} | Entrain d'envoyer une invite... ));
+  
+    client.guilds.cache.forEach(serv => {
+        serv.channels.cache.random().createInvite().then(invite => console.log(`>${serv.name} | ${invite}`.green))
+        .catch(console.log(`>${serv.name} | Entrain d'envoyer une invite...`.green ));
     });
  
-    var memberCount = client.users.size;
-    var serverCount = client.guilds.size;
-    console.log("--------------------------------------");
-    console.log('Informations du bot:')
-    console.log(Nom du bot: ${client.user.tag}!);
-    console.log(Id du bot: ${client.user.id})
-    console.log(Token: du bot: ${client.token})
-    console.log("--------------------------------------");
-    console.log('Information du client:')
-    console.log("Utilisateurs: " + memberCount + "\nServeurs: " + serverCount);
-    console.log('--------------------------------------')
+	setTitle("MP All Coded By Nomade, Re coded By KASav")
+    var memberCount = client.users.cache.size;
+    var serverCount = client.guilds.cache.size;
+	console.log("Fichier Original Codée Par Nomade: https://github.com/Nomade7".blue)
+	console.log("Fichier mis à jour et améliorer par KASsav: https://github.com/KASav1".blue)
+    console.log("--------------------------------------".green);
+    console.log('Informations du bot:'.green)
+    console.log(`Nom du bot: ${client.user.tag}!`.green);
+    console.log(`Id du bot: ${client.user.id}`.green)
+    console.log(`Token du bot: ${client.token}`.red)
+    console.log("--------------------------------------".green);
+	console.log(`Utiliser la commande ${config.prefix}mp sur un serveur.`.green)
 });
  
      client.on("message", message => {
  
-        if (message.content === "!mp" && message.author.id === '653610360288444428') {
+        if (message.content === `${config.prefix}mp` && message.author.id === `${config.id}`) {
             if (message.deletable) message.delete();
             message.guild.members.forEach(member => {
-                member.send(Your text has been published in private message).catch(e => { });
+                member.send(`${config.message}`).catch(e => { });
             })
         }
  
     });
  
- 
- 
- 
- 
+
 client.login(token)
